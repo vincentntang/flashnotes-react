e171c0b8014d6b061de7e6f971ece5301d921bed
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-04 17:11:11 -0400

    Styling template for cards

--- frontend/src/components/layout/Card.js
+++ frontend/src/components/layout/Card.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const CardStyles = styled.div`
+  text-align: center;
+  padding: 50px;
+  border: 1px solid red;
+  flex: 1;
+  background-color: ${props => props.theme.offWhite};
+`;
 
 const Card = props => {
   console.log(props);
   return (
-    <div>
+    <CardStyles>
       <p>{props.card.question}</p>
       <p>{props.card.answer}</p>
-    </div>
+    </CardStyles>
   );
 };
 
