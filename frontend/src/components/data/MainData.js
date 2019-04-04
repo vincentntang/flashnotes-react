f6f11277f0678de41aad8ef25302c4a6e9b14603
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-04 14:55:28 -0400

    Remove HTML from data res, set to Q/A

--- frontend/src/components/data/MainData.js
+++ frontend/src/components/data/MainData.js
@@ -3,15 +3,18 @@ const MainData = {
   cards: [
     {
       id: "1",
-      content: "<p>I'm ID one</p><p>Lorem ipsum</p>"
+      question: "I'm question one",
+      answer: "I'm answer one"
     },
     {
       id: "2",
-      content: "<p>I'm ID two</p><p>Lorem Ipsum</p>"
+      question: "I'm question two",
+      answer: "I'm answer two"
     },
     {
       id: "3",
-      content: "<p>I'm ID three</p><p>Lorem Ipsum</p>"
+      question: "I'm question two",
+      answer: "I'm answer two"
     }
   ]
 };
