d7c63a605544cb727d61b4c9efb9254d302c4091
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-05 16:46:10 -0400

    Simple Navbar Styling

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -4,26 +4,45 @@ import { Link } from "react-router-dom";
 
 const NavbarStyles = styled.nav`
   background-color: ${props => props.theme.grey}
-  text-align: center;
-  padding: 30px;
+  width: 100%;
+  display: flex;
+  padding: 10px;
+  .home {
+    flex-basis: 120px;
+  }
+  ul {
+    padding-left: 0px;
+    margin: 0px;
+  }
+  ul {
+    display:flex;
+    justify-content: flex-end;
+    width: 100%;
+  }
+  ul li {
+    list-style-type:none;
+    margin-left: 10px;
+  }
 `;
 
 export default class Navbar extends Component {
   render() {
     return (
-      <NavbarStyles className="container">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Register">Register</Link>
-          </li>
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-        </ul>
-      </NavbarStyles>
+      <div className="container">
+        <NavbarStyles>
+          <Link to="/" className="home">
+            Flash Notes
+          </Link>
+          <ul>
+            <li>
+              <Link to="/Register">Register</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+          </ul>
+        </NavbarStyles>
+      </div>
     );
   }
 }
