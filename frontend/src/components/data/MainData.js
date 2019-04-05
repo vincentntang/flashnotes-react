6f6c61bba20ad8a63e2e782deb8481c3ad9b6c7b
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-05 19:39:15 -0400

    Added mocked data for decks

--- frontend/src/components/data/MainData.js
+++ frontend/src/components/data/MainData.js
@@ -15,6 +15,23 @@ const MainData = {
       id: "ID3",
       question: "I'm question three",
       answer: "I'm answer three"
+    },
+    {
+      id: "ID4",
+      question: "I'm question four",
+      answer: "I'm answer four"
+    }
+  ],
+  decks: [
+    {
+      id: "Deck1",
+      title: "Javascript Deck",
+      cardIds: ["ID1", "ID2"]
+    },
+    {
+      id: "Deck2",
+      title: "PHP Deck",
+      cardIds: ["ID3,ID4"]
     }
   ]
 };
