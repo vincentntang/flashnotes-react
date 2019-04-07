4d3e2d5a98639506e21ab85fbadec9c65708c08e
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-07 01:13:54 -0400

    scaffolding note data and updating deck data

---
+++ frontend/src/components/data/MainData2.js
@@ -0,0 +1,51 @@
+// Mocked Data Response
+const MainData = {
+  cards: [
+    {
+      id: "ID1",
+      question: "I'm question one",
+      answer: "I'm answer one",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
+    },
+    {
+      id: "ID2",
+      question: "I'm question two",
+      answer: "I'm answer two",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
+    },
+    {
+      id: "ID3",
+      question: "I'm question three",
+      answer: "I'm answer three",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
+    },
+    {
+      id: "ID4",
+      question: "I'm question four",
+      answer: "I'm answer four",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
+    }
+  ],
+  decks: [
+    {
+      id: "Deck1",
+      title: "Javascript Deck",
+      cardIds: ["ID1", "ID2"]
+    },
+    {
+      id: "Deck2",
+      title: "PHP Deck",
+      cardIds: ["ID3,ID4"]
+    }
+  ]
+};
+
+export default MainData;
