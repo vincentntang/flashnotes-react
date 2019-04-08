8799ead2dbe2b5b61506ebb210eda2b226d0cce2
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-07 22:55:32 -0400

    convert MainData

--- frontend/src/components/data/MainData.js
+++ frontend/src/components/data/MainData.js
@@ -4,34 +4,46 @@ const MainData = {
     {
       id: "ID1",
       question: "I'm question one",
-      answer: "I'm answer one"
+      answer: "I'm answer one",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
     },
     {
       id: "ID2",
       question: "I'm question two",
-      answer: "I'm answer two"
+      answer: "I'm answer two",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
     },
     {
       id: "ID3",
       question: "I'm question three",
-      answer: "I'm answer three"
+      answer: "I'm answer three",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
     },
     {
       id: "ID4",
       question: "I'm question four",
-      answer: "I'm answer four"
+      answer: "I'm answer four",
+      createdAt: "DateTime",
+      modifiedAt: "DateTime",
+      deck: "DeckID"
     }
   ],
   decks: [
     {
       id: "Deck1",
-      title: "Javascript Deck",
-      cardIds: ["ID1", "ID2"]
+      title: "Javascript Deck"
+      // cardIds: ["ID1", "ID2"]
     },
     {
       id: "Deck2",
-      title: "PHP Deck",
-      cardIds: ["ID3,ID4"]
+      title: "PHP Deck"
+      // cardIds: ["ID3,ID4"]
     }
   ]
 };
--- frontend/src/components/data/MainDataV1.js
+++ frontend/src/components/data/MainDataV1.js
@@ -4,34 +4,22 @@ const MainData = {
     {
       id: "ID1",
       question: "I'm question one",
-      answer: "I'm answer one",
-      createdAt: "DateTime",
-      modifiedAt: "DateTime",
-      deck: "DeckID"
+      answer: "I'm answer one"
     },
     {
       id: "ID2",
       question: "I'm question two",
-      answer: "I'm answer two",
-      createdAt: "DateTime",
-      modifiedAt: "DateTime",
-      deck: "DeckID"
+      answer: "I'm answer two"
     },
     {
       id: "ID3",
       question: "I'm question three",
-      answer: "I'm answer three",
-      createdAt: "DateTime",
-      modifiedAt: "DateTime",
-      deck: "DeckID"
+      answer: "I'm answer three"
     },
     {
       id: "ID4",
       question: "I'm question four",
-      answer: "I'm answer four",
-      createdAt: "DateTime",
-      modifiedAt: "DateTime",
-      deck: "DeckID"
+      answer: "I'm answer four"
     }
   ],
   decks: [
