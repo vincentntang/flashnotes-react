7b29497edc35cd59df8db91f08055a68e4b80a49
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-08 22:00:22 -0400

    playing around with logic

--- frontend/src/components/deck/Review.js
+++ frontend/src/components/deck/Review.js
@@ -5,36 +5,86 @@ import ReviewItem from "./ReviewItem";
  * Review containes all the logic for handling cards
  * Has card and state
  */
+
+// STATES
+const views = {
+  CARD: "CARD",
+  DONE: "DONE"
+};
+
 export default class Review extends Component {
   state = {
-    count: 0
+    count: 0,
+    views: views.CARD,
+    cards: [] // filtered data from props
   };
-  componentDidMount() {}
+  componentDidMount() {
+    // Normally, you would axios call only the data you need
+    // but in this case I filtered out all the props to the deck I want
+    // axios dispatch to GET data
+    this.setState({
+      cards: this.props.cards.filter(obj => obj.deck === this.props.deckID)
+    });
+  }
+  componentDidUpdate() {
+    // reserved when I need to push back the result,
+    // or dispatch an action to POST back data
+  }
   onSuccess = () => {
     this.setState({ count: this.state.count++ });
   };
-  render() {
-    const { deckID, cards, decks } = this.props;
-    let { count } = this.state;
-    // shouldn't be loading the entire data. Just, specific items
-    let cardsInDeck = cards.filter(obj => obj.deck === deckID);
-    const numCards = cardsInDeck.length;
-    let cardContent;
-    console.log(numCards);
-    let i = 0;
-
-    let succeed = false;
-    // Traverse cards((
-    for (let i = 0; i < numCards; i++) {
-      cardContent = (
+  showCard = (cards, count) => {
+    return (
+      <div>
+        hey
         <ReviewItem
-          question={cardsInDeck[i].question}
-          answer={cardsInDeck[i].answer}
+          question={cards[count].question}
+          answer={cards[count].answer}
           onSuccess={this.onSuccess}
         />
+      </div>
+    );
+  };
+  renderCards = () => {
+    const { deckID } = this.props;
+    let { count, views, cards } = this.state;
+
+    // Iterate, use recursion
+    if (count < cards.length) {
+      // Define a seperate function so logic is easier
+      // this.showCard(cards, count);
+      return (
+        <div>
+          hey
+          <ReviewItem
+            question={cards[count].question}
+            answer={cards[count].answer}
+            onSuccess={this.onSuccess}
+          />
+        </div>
       );
-      setTimeout(5000);
     }
-    return <div>{cardContent}</div>;
+  };
+  render() {
+    // let { count } = this.state;
+    // // shouldn't be loading the entire data. Just, specific items
+    // let cardsInDeck = cards.filter(obj => obj.deck === deckID);
+    // const numCards = cardsInDeck.length;
+    // let cardContent;
+    // console.log(numCards);
+    // let i = 0;
+
+    // let succeed = false;
+    // // Traverse cards((
+    // for (let i = 0; i < numCards; i++) {
+    //   cardContent = (
+    //     <ReviewItem
+    //       question={cardsInDeck[i].question}
+    //       answer={cardsInDeck[i].answer}
+    //       onSuccess={this.onSuccess}
+    //     />
+    //   );
+    // }
+    return <div>{this.renderCards()}</div>;
   }
 }
