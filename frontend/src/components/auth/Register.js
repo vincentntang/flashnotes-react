9f6b7a14a8542026a954a341e9d4425dd9e8b414
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-11 12:37:36 -0400

    Added Register Placeholder

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,7 +1,70 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import TextFieldGroup from "../common/TextFieldGroup";
 
-export default class Register extends Component {
+const StyledRegister = styled.section`
+  text-align: center;
+`;
+const StyledHeader = styled.div``;
+const StyledBody = styled.div``;
+
+class Login extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+    errors: {}
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: [e.target.value]
+    });
+  };
   render() {
-    return <div>Register</div>;
+    const { errors } = this.state;
+    return (
+      <StyledRegister>
+        <StyledHeader>
+          <h1>Register</h1>
+        </StyledHeader>
+        <StyledBody>
+          <TextFieldGroup
+            placeholder="Name"
+            name="name"
+            type="name"
+            value={this.state.name}
+            onChange={this.onChange}
+            error={errors.name}
+          />
+          <TextFieldGroup
+            placeholder="Email Address"
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            error={errors.email}
+          />
+          <TextFieldGroup
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.onChange}
+            error={errors.password}
+          />
+          <TextFieldGroup
+            placeholder="Confirm Password"
+            name="password2"
+            type="password"
+            value={this.state.password2}
+            onChange={this.onChange}
+            error={errors.password2}
+          />
+        </StyledBody>
+      </StyledRegister>
+    );
   }
 }
+
+export default Login;
