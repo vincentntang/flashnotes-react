1b4608a558d8c8e0beaa12eb4cb6ec91050586fb
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-15 15:52:59 -0400

    added card schema

--- backend/models/Card.js
+++ backend/models/Card.js
@@ -1,1 +1,36 @@
-// placeholder
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const CardSchema = new Schema(
+  {
+    question: {
+      type: String,
+      required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    },
+    deck: {
+      type: Schema.Types.ObjectId,
+      ref: "decks"
+    },
+    difficulty: {
+      type: Number,
+      required: true
+    },
+    daysBetweenReviews: {
+      type: Number,
+      required: true
+    },
+    dateLastReviewed: {
+      type: Date
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = User = mongoose.model("cards", UserSchema);
