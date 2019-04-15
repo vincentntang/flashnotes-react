1b11a586921c209c2e879da6b12b892604336c5b
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-15 13:13:26 -0400

    Added user routes and validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
-// Load user model
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+// Load User model
 const User = require("../../models/User");
 
-/**
- * Router appends additional routing information. These two are the same:
- * app.use("/api/users", users);
- * app.use("/api/users", router.get("/test", (req, res) => res.json({ msg: "User Works" }))
- * http://localhost:5000/api/users/test
- */
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get("/test", (req, res) => res.json({ msg: "User Works" }));
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 // @route   GET api/users/register
 // @desc    Register user
-// @access  Publicrouter.post("/register", (req, res) => {
+// @access  Public
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body); // this checks password2
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = "Email already exists";
       return res.status(400).json(errors);
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
+        avatar,
         password: req.body.password
       });
 
@@ -43,5 +60,68 @@ router.get("/test", (req, res) => res.json({ msg: "User Works" }));
     }
   });
 });
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
