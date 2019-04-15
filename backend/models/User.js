ad311c799b4747f24f0920c804f01db87703a906
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-15 13:29:23 -0400

    added bearer token on login

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 // Create Schema
-var UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  avatar: { type: String },
-  date: { type: Date, default: Date.now }
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
-module.exports = User = mongoose.model("user", UserSchema);
+
+module.exports = User = mongoose.model("users", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,40 +2,39 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require("path"); // part of nodeJS core lib
 
+const users = require("./routes/api/users");
 const cards = require("./routes/api/cards");
 const decks = require("./routes/api/decks");
 const notes = require("./routes/api/notes");
-const users = require("./routes/api/users");
 
-// Define express
 const app = express();
 
-// body parser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// // Connect database
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-// // Connect to mongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 // Passport middleware
-// app.use(passport.initialize());
+app.use(passport.initialize());
 
-// // Passport config
-// require("./config/passport")(passport);
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
-
-const router = express.Router();
+app.use("/api/cards", cards);
+app.use("/api/decks", decks);
+app.use("/api/notes", notes);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Serving running on ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
