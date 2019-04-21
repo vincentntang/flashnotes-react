56f59b7d259e79737b06a9f94dcb7e92fe19bf34
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-04-21 08:38:10 -0400

    push to decks on login

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -20,13 +20,13 @@ class Login extends Component {
 
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/dashboard");
+      this.props.history.push("/decks");
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/dashboard");
+      this.props.history.push("/decks");
     }
 
     if (nextProps.errors) {
